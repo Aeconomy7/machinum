23bf43e5faa2422875590b35574ca4f17cc3a118
aeconomy7 <aeconomy7@gmail.com>
2018-09-13 13:56:38 -0500

    First commit, version 0.01 (does a whole lotta nuthin)

---
+++ machinum.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# M A C H I N U M
+# Script to enumerate a Linux machine
+# created by Sc00by
+
+# Colours ~makeitgood~
+NORMAL=$(echo -e "\033[1;0m")
+GREEN=$(echo -e "\033[1;32m")
+BLUE=$(echo -e "\033[1;34m")
+MAGENTA=$(echo -e "\033[1;35m")
+CYAN=$(echo -e "\033[1;36m")
+
+# DONT LOOK AT THIS HIDEOUSNESS
+echo "$BLUE"
+echo    "<>~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~<>"
+echo    "||                                   ||"
+echo -n "||"
+echo -n "$GREEN"
+echo -n "           Machinum v0.01"
+echo -n "$BLUE"
+echo    "          ||"
+echo -n "||"
+echo -n "$CYAN"
+echo -n "              by Sc00by"
+echo -n "$BLUE"
+echo    "            ||"
+echo    "||                                   ||"
+echo    "<>~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~<>"
+# END HIDEOUSNESS
