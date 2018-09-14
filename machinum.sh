171c99eca2b256cca705367f10592027879a9d4c
aeconomy7 <aeconomy7@gmail.com>
2018-09-14 00:08:32 -0500

    Added more features

--- machinum.sh
+++ machinum.sh
@@ -28,3 +28,97 @@ echo    "            ||"
 echo    "||                                   ||"
 echo    "<>~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~<>"
 # END HIDEOUSNESS
+
+echo "$GREEN"
+echo -e "Start time:"
+echo -ne "$NORMAL\t"
+date
+
+# # # S Y S T E M # # #
+echo "$BLUE"
+echo -n "<>~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~<>"
+echo "$GREEN"
+echo -n "#"
+echo -n "$NORMAL"
+echo -n "~~~~~~~~~~~~~~~"
+echo -n "$GREEN"
+echo -n " SYSTEM "
+echo -n "$NORMAL"
+echo -n "~~~~~~~~~~~~~~~"
+echo -n "$GREEN"
+echo "#"
+echo -n "$BLUE"
+echo    "<>~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~<>"
+
+# current userid
+curruser=`whoami 2>/dev/null`
+if [ "$curruser" ]; then
+	echo "$MAGENTA"
+	echo -n "Effective userid: "
+	echo "$NORMAL"
+	echo -e "\t$curruser"
+fi
+
+# system information
+gensysinfo=`uname -a 2>/dev/null`
+if [ "$gensysinfo" ]; then
+	echo "$MAGENTA"
+	echo -n "System information: "
+	echo "$NORMAL"
+	echo -e "\t$gensysinfo"
+fi
+
+# Kernal information
+procv=`cat /proc/version 2>/dev/null`
+if [ "$procv" ]; then
+	echo "$MAGENTA"
+	echo -n "Extended kernel information:"
+	echo "$NORMAL"
+	echo -e "\t$procv"
+fi
+
+# Hostname of current machine
+currhost=`hostname 2>/dev/null`
+if [ "$currhost" ]; then
+	echo "$MAGENTA"
+	echo -n "Effective hostname: "
+	echo "$NORMAL"
+	echo -e "\t$currhost"
+fi
+
+# # # # U S E R ? G R O U P # # # #
+
+echo
+echo "$BLUE"
+echo -n "<>~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~<>"
+echo "$GREEN"
+echo -n "#"
+echo -n "$NORMAL"
+echo -n "~~~~~~~~~~~~~~~"
+echo -n "$GREEN"
+echo -n " USER/GROUP "
+echo -n "$NORMAL"
+echo -n "~~~~~~~~~~~~~~~"
+echo -n "$GREEN"
+echo "#"
+echo -n "$BLUE"
+echo    "<>~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~<>"
+
+
+# current userid (id-wise)
+userid=`id 2>/dev/null`
+if [ "$userid" ]; then
+	echo "$MAGENTA"
+	echo -n "User details: "
+	echo "$NORMAL"
+	echo -e "\t$userid"
+fi
+
+# last logged on user
+lastlog=`lastlog | grep "Never" 2>/dev/null`
+if [ "$lastlog" ]; then 
+	echo "$MAGENTA"
+	echo -n "Last user logged on: "
+	echo "$NORMAL"
+	echo -e "\t$lastlog"
+fi
